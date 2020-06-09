8db3bf6670bb638c5db03d9836f6ba970488d1b8
soywillians9 <2342193350101@ingenieria.usac.edu.gt>
2020-06-09 12:47:34 -0600

    Entrega

---
+++ corto1.py
@@ -0,0 +1,14 @@
+def seccollatz(num):
+
+    if num%2 == 0:
+        num = num // 2
+        print("num")
+        num = int
+    else:
+        number = number * 3 + 1
+        print("num")
+
+
+
+for i in range(2,671):
+    print ("num")
